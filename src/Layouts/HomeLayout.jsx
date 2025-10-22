861386c384cfa68bdb8540fb9a7db1cc4dd646c9
Fahim2023 <fahimshahrier2023@gmail.com>
2025-10-22 13:07:22 +0600

    added header & footer

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Header from "../Components/Header";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
 
 const HomeLayout = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <Header></Header>
+      </header>
+      <main>
+        <section>
+          <Outlet></Outlet>
+        </section>
+      </main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
 };
 
 export default HomeLayout;
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,3 +1,21 @@
 import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layouts/HomeLayout";
+import Home from "../Components/Home";
 
-const router = createBrowserRouter();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomeLayout,
+    children: [
+      {
+        path: "",
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <h2>error 404</h2>,
+  },
+]);
+export default router;
