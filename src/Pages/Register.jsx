d087b9084e94b3a9f7acf3033c3cb59badd07732
Fahim2023 <fahimshahrier2023@gmail.com>
2025-10-23 00:45:53 +0600

    added register & login functions of firebase

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { use } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router";
+import { AuthContext } from "../Provider/AuthContext";
 
 const Register = () => {
+  const { createUser } = use(AuthContext);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // const name = e.target.name.value;
+    // const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl ">
@@ -10,20 +26,40 @@ const Register = () => {
           <h1 className="text-2xl md:text-3xl font-bold text-center">
             Signup!
           </h1>
-          <form>
+          <form onSubmit={handleRegister}>
             <fieldset className="fieldset">
               {/* Name */}
               <label className="label">Name</label>
-              <input type="text" className="input" placeholder="Name" />
+              <input
+                name="name"
+                type="text"
+                className="input"
+                placeholder="Name"
+              />
               {/* Photo URL */}
               <label className="label">Photo URL</label>
-              <input type="text" className="input" placeholder="photo URL" />
+              <input
+                name="photo"
+                type="text"
+                className="input"
+                placeholder="photo URL"
+              />
               {/* Email */}
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                name="email"
+                type="email"
+                className="input"
+                placeholder="Email"
+              />
               {/* Password */}
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                name="password"
+                type="password"
+                className="input"
+                placeholder="Password"
+              />
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../Firebase/Firebase.config";
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
   const authData = {
     user,
     setUser,
+    createUser,
+    signInUser,
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
