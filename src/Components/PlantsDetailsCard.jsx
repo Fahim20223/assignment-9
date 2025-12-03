7d9c92ba03d33d842e3c13b4fe3eb81139ed6c0d
Fahim2023 <fahimshahrier2023@gmail.com>
2025-12-03 21:30:13 +0600

    added user frindly back button to the details page

--- src/Components/PlantsDetailsCard.jsx
+++ src/Components/PlantsDetailsCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const PlantsDetailsCard = ({ plants }) => {
+  const navigate = useNavigate();
   return (
     <div className="min-h-[63vh] bg-[#f6f6f6] py-12">
       <div className="max-w-7xl mx-auto p-4 md:p-6 lg:p-8">
@@ -20,7 +22,6 @@ const PlantsDetailsCard = ({ plants }) => {
               <h1 className="text-3xl md:text-4xl font-bold text-green-600">
                 {plants.plantName}
               </h1>
-
               <div className="flex gap-3 flex-wrap">
                 <div className="badge badge-lg badge-outline text-green-600 border-green-600 font-medium">
                   Category: {plants.category}
@@ -32,15 +33,12 @@ const PlantsDetailsCard = ({ plants }) => {
                   Stock: {plants.availableStock}
                 </div>
               </div>
-
               <p className="text-gray-700 font-medium text-lg mt-4">
                 {plants.description}
               </p>
-
               <p className="text-xl font-semibold text-green-600">
                 Price: 💲{plants.price}
               </p>
-
               <div className="flex gap-2 flex-wrap mt-2">
                 <div className="badge badge-outline text-blue-600 border-blue-600">
                   Care Level: {plants.careLevel}
@@ -49,6 +47,12 @@ const PlantsDetailsCard = ({ plants }) => {
                   Provider: {plants.providerName}
                 </div>
               </div>
+              <button
+                onClick={() => navigate(-1)}
+                className="btn bg-green-600 text-white w-1/2 rounded-lg"
+              >
+                Back
+              </button>
             </div>
           </div>
         </div>
